b571d9f97f7d09a29b089bac59f88f2601d3a000
ZainabAlawadh <zainabalawadh0@gmail>
2020-06-15 13:28:09 +0300

    done HW1😎

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -7,16 +7,16 @@
 
 let name = "Ahmad"
 
-name = "Salem"
+let name2 = "Salem"
 
 
-var myWeight = 60
-var FriendWeight = 80.5
-var height = 1.5
+var myWeight =  60
+var FriendWeight = Int(80.5)
+var height = Int(1.5)
 
 
 var weightSum = myWeight + FriendWeight
-
+//gg
 
 
 var bmi = myWeight * (height * height)
@@ -38,28 +38,38 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "Mariam"
+var lastName1 = "Alawadh"
+var email1 = "malawadh@hotmail.com"
+var phoneNumber1 = "99887766"
+var age1 = 6
+var country1 = "kuwait"
+var area1 = "faiha"
+var password1 = "223344"
+var isKuwaiti1 = true
 
 //Person 2
-
-
-
-
-
-
-
-
+var FirstName2 = "Aminah"
+var lastName2 = "jassem"
+var email2 = "aminah@gmail.com"
+var phoneNumber2 = "55667788"
+var age2 = 6
+var country2 = "kuwait"
+var area2 = "faiha"
+var password2 = "778899"
+var isKuwaiti2 = false
 
 
 //Person 3
+var firstName3 = "saleh"
+var lastName3 = "Abdullah"
+var email3 = "abdullah@gmail.com"
+var phoneNumber3 = "9786546"
+var age3 = 19
+var country3 = "kuwait"
+var area3 = "Rawda"
+var password3 = "778899"
+var isKuwaiti3 = true
 
 
 
@@ -82,3 +92,56 @@ var isKuwaiti = false
 
  
  */
+
+firstName1 != FirstName2
+FirstName2 != firstName3
+firstName1 != firstName3
+
+lastName1 != lastName2
+lastName2 != lastName3
+lastName1 != lastName3
+
+email1 != email2
+email2 != email3
+email3 != email1
+
+phoneNumber1 != phoneNumber2
+phoneNumber2 != phoneNumber3
+phoneNumber1 != phoneNumber3
+
+age1 == age2
+age2 < age3
+age1 < age3
+
+country1 == country2
+country2 == country3
+country1 == country3
+
+area1 == area2
+area2 != area3
+area3 != area1
+
+password1 == password2
+password2 == password3
+password1 == password3
+isKuwaiti1 != isKuwaiti2
+isKuwaiti2 != isKuwaiti3
+isKuwaiti3 == isKuwaiti1
+
+age1 > 18
+age1 < 18
+
+age2 > 18
+age2 < 18
+
+age3 > 18
+age3 < 18
+
+age1 == 17
+age1 != 17
+
+age2 == 17
+age2 != 17
+
+age3 == 17
+age3 != 17
